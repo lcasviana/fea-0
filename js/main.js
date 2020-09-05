01781fa46e7b14bb7b6dcd4b1c8532a379320d87
Lucas Viana <lcasviana@gmail.com>
2020-09-05 01:54:04 -0300

    Saving name on localstorage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let nome = '';
+
 function helloWorld() {
   for (let i = 3; i > 0; i--) {
     console.log(i);
@@ -6,3 +8,19 @@ function helloWorld() {
   console.log('Hello World!');
 }
 helloWorld();
+
+function inserirOuAtualizarNome() {
+  nome = localStorage.getItem('nome');
+  if (nome) {
+    if (confirm(`${nome}, deseja trocar o seu nome?`)) {
+      nome = prompt('Digite seu novo nome:');
+      localStorage.setItem('nome', nome);
+    }
+  } else {
+    nome = prompt('Cadastre seu nome:');
+    localStorage.setItem('nome', nome);
+  }
+  document.querySelector('#nome').innerHTML = `Bem vindo, ${nome}!`;
+}
+
+inserirOuAtualizarNome();

4796907c862223a4aeb0719b8b042bf2220abf47
Lucas Viana <lcasviana@gmail.com>
2020-09-12 21:30:35 -0300

    Using class

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,57 +1,61 @@
-let notes;
+class Notes {
+  notes;
 
-function init() {
-  notes = JSON.parse(localStorage.getItem('notes') || '[]');
-  render();
-}
+  constructor() {
+    this.notes = JSON.parse(localStorage.getItem('notes') || '[]');
+    this.render();
+  }
 
-function save() {
-  localStorage.setItem('notes', JSON.stringify(notes));
-}
+  render() {
+    const notesClear = document.querySelectorAll('.note');
+    notesClear.forEach(note => note.remove());
 
-function render() {
-  const notesClear = document.querySelectorAll('.note');
-  notesClear.forEach(note => note.remove());
+    const notesElement = document.querySelector('.notes');
+    this.notes.forEach((note, index) => notesElement.appendChild(this.createNode(note, index)));
+  }
 
-  const notesElement = document.querySelector('.notes');
-  notes.forEach((note, index) => {
+  save() {
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+
+  add() {
+    this.notes.push({ title: '', description: '' });
+    this.save();
+    this.render();
+  }
+
+  del(index) {
+    if (confirm('Confirmar deleção?')) {
+      this.notes = this.notes.filter((_, i) => index !== i);
+      this.save();
+      this.render();
+    }
+  }
+
+  createNode(note, index) {
     const noteElement = document.createElement('div');
     noteElement.className = 'card note';
     noteElement.innerHTML = `
       <div class="card-header">
-        <input type="text" class="form-control" onblur="setTitle(${index}, this.value)" value="${note.title}">
+        <input type="text" class="form-control" onblur="notes.setTitle(${index}, this.value)" value="${note.title}">
       </div>
       <div class="card-body">
-        <textarea class="form-control" rows="4" onblur="setDesc(${index}, this.value)">${note.description}</textarea>
+        <textarea class="form-control" rows="4" onblur="notes.setDesc(${index}, this.value)">${note.description}</textarea>
       </div>
-      <button class="btn btn-light" onclick="del(${index})" title="Deletar anotação"> ✖ </button>
+      <button class="btn btn-light" onclick="notes.del(${index})" title="Deletar anotação"> ✖ </button>
     `;
-    notesElement.appendChild(noteElement);
-  });
-}
+    return noteElement;
+  }
 
-function add() {
-  notes.push({ title: '', description: '' });
-  save();
-  render();
-}
-
-function del(index) {
-  if (confirm('Confirmar deleção?')) {
-    notes = notes.filter((_, i) => index !== i);
+  setTitle(index, title) {
+    this.notes = this.notes.map((note, i) => index !== i ? note : { ...note, title });
     save();
-    render();
+  }
+
+  setDesc(index, description) {
+    this.notes = this.notes.map((note, i) => index !== i ? note : { ...note, description });
+    this.save();
   }
 }
 
-function setTitle(index, title) {
-  notes = notes.map((note, i) => index !== i ? note : { ...note, title });
-  save();
-}
-
-function setDesc(index, description) {
-  notes = notes.map((note, i) => index !== i ? note : { ...note, description });
-  save();
-}
-
-init();
+const notes = new Notes();
